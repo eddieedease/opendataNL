9cb7d5d48186c60d79efa728ad12fb912ecb409e
eddieedease <eddie@edease.nl>
2021-10-28 14:06:49 +0200

    all the nessys added for startings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,63 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import {
+  DomSanitizer
+} from '@angular/platform-browser';
+
+import {
+  RouterModule,
+  Routes
+} from '@angular/router';
+import {
+  FormsModule
+} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { ContactComponent } from './contact/contact.component';
+
+import {
+  ServiceService
+} from './service.service';
+
+// import evironment for current dev bunlde
+import {
+  environment
+} from '../environments/environment';
+
+const appRoutes: Routes = [{
+  path: '',
+  component: LandingComponent
+},
+{
+  path: 'contact',
+  component: ContactComponent
+}]
+;
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    ContactComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes, {
+        useHash: false,
+        onSameUrlNavigation: 'reload'
+      }, // <-- debugging purposes only
+    ),
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+
+ }
